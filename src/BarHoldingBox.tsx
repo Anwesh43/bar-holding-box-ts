4d30eb01757da0a280cafb42d3d72a289b13f27b
Anwesh43 <anweshthecool0@gmail.com>
2022-01-18 00:27:32 +0530

    adding BarHoldingBox component

---
+++ src/BarHoldingBox.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useStyle } from './hooks'
+import withContext from './withContext'
+
+interface BHBProps {
+    w : number, 
+    h : number, 
+    scale : number, 
+    onClick : Function 
+}
+
+const BarHoldingBox = (props : BHBProps) => {
+    const {barStyle, boxStyle, parentStyle} = useStyle(props.w, props.h, props.scale)
+    return (
+        <React.Fragment>
+            <div style = {parentStyle()}>
+                <div style = {barStyle()}></div>
+                {[0, 1].map(i => (<div key = {`box_${i}`} style = {boxStyle(i)}></div>))}
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default BarHoldingBox 
